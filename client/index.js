88a1bbfb7833a76e639431200e0aff5046c51912
Kiril <kiril.zhelyazkov9@gmail.com>
2017-10-01 19:43:19 +0300

    added the upload button, but still missing props validation and jsdocs

--- client/index.js
+++ client/index.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Button from 'react-bootstrap/lib/Button';
-import { FormGroup, InputGroup, FormControl } from 'react-bootstrap';
+// import Button from 'react-bootstrap/lib/Button';
+import { FormGroup, ControlLabel, FormControl, HelpBlock } from 'react-bootstrap';
+
+function FieldGroup({ id, label, help, ...props }) {
+  return (
+    <FormGroup controlId={id}>
+      <ControlLabel>{label}</ControlLabel>
+      <FormControl {...props} />
+      {help && <HelpBlock>{help}</HelpBlock>}
+    </FormGroup>
+  );
+}
 
 const element = (
   <div>
-    <FormGroup>
-      <InputGroup>
-        <InputGroup.Button>
-          <Button>Before</Button>
-        </InputGroup.Button>
-        <FormControl type="text" />
-      </InputGroup>
-    </FormGroup>
-    <h1>Hello, world!</h1>
-    <Button>Default</Button>
+    <h1>WebArch!</h1>
+    <FieldGroup
+      id="formControlsFile"
+      type="file"
+      label="File"
+      help="Example block-level help text here."
+    />
   </div>
 );
 
