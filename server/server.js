e1d86880c9111b7d6787c98828a79c81a81ddebe
Kiril <kiril.zhelyazkov9@gmail.com>
2017-10-01 19:44:53 +0300

    added comments and refactored code

--- server/server.js
+++ server/server.js
@@ -7,13 +7,13 @@ const path = require('path');
 const PORT = 8888;
 const app = express();
 
+// takes the bundled js from webpack and sends it to the server
 app.use('/build', express.static(path.join(__dirname, '../build')));
 
-
+// loads the main HTML of the project
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../index.html'));
 });
-
 
 app.listen(PORT);
 // module.exports.start = start;
