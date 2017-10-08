539c52fb76563b18ee7e302ee4d3233c8f05972f
Kiril <kiril.zhelyazkov9@gmail.com>
2017-10-08 17:31:11 +0300

    basic database logic that inserts schema every time its loaded

--- server/server.js
+++ server/server.js
@@ -1,11 +1,36 @@
-const http = require('http');
 const express = require('express');
-const fs = require('fs');
 const path = require('path');
-// const url = require('url');
+const mongoose = require('mongoose');
 
 const PORT = 8888;
 const app = express();
+const mongoDB = 'mongodb://localhost:27017/test';
+const Schema = mongoose.Schema;
+const usersSchema = new Schema({
+  name: String,
+  password: String,
+  email: String,
+});
+
+const Users = mongoose.model('Users', usersSchema);
+const firstUser = new Users({
+  name: 'Kiril',
+  password: 'hash',
+  email: 'knz140@aubg.edu',
+});
+firstUser.save((err) => {
+  if (err) console.log(err);
+});
+
+mongoose.connect(mongoDB, {
+  useMongoClient: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error'));
+db.once('open', () => {
+  console.log('connected');
+});
 
 // takes the bundled js from webpack and sends it to the server
 app.use('/build', express.static(path.join(__dirname, '../build')));
@@ -13,6 +38,12 @@ app.use('/build', express.static(path.join(__dirname, '../build')));
 // loads the main HTML of the project
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../index.html'));
+});
+
+app.get('/users', (req, res) => {
+  mongoose.model('Users').find((err, users) => {
+    res.send(users);
+  });
 });
 
 app.listen(PORT);
